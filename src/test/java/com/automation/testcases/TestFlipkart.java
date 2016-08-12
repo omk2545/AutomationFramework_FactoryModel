844192751f72b3fd093fe85981d13a1d4355c5c7
omkar Kulkani <omk2545@gmail.com>
2016-08-13 00:15:22 +0530

    Added the changes for Before and After  Methd
    Added Before and After method

--- src/test/java/com/automation/testcases/TestFlipkart.java
+++ src/test/java/com/automation/testcases/TestFlipkart.java
@@ -3,6 +3,8 @@ package com.automation.testcases;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.automation.helper.Constants;
@@ -11,14 +13,19 @@ import com.automation.pages.LandingPage;
 import com.automation.pages.ProductsDisplay;
 
 public class TestFlipkart {
+	
+	
+public WebDriver driver;
+
+	@BeforeTest
+	public void init() {
+		driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+	}
 
 	@Test
 	public void test01() {
-
-		WebDriver driver;
-		driver = new FirefoxDriver();
-		driver.manage().window().maximize();
-
+		
 		LandingPage landingPage = PageFactory.initElements(driver,
 				LandingPage.class);
 		
@@ -28,18 +35,16 @@ public class TestFlipkart {
 		Object Page = displayPage.findProduct(Constants.validProduct);
 		if (Page instanceof ProductsDisplay) {
 			System.out.println("We are on the Product Display ");
-
 		} else {
-
 			System.out.println("We are on the Display page only ");
-			
 		}
 
-		
 		Object Page2 = ((ProductsDisplay) Page).selectFirstProductFromList();
-		 
-		
-		
+	}
+	
+	@AfterTest
+	public void Teardown(){
+	driver.quit();
 	}
 
 }
